7e2b2717aad43c13686c9eb7fd0b4ea090151e67
Akram908 <mdakrammunshi39@gmail.com>
2022-05-05 14:18:00 +0530

    ui changes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,4 @@
 import {NgModule} from '@angular/core';
-import {A11yModule} from '@angular/cdk/a11y';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {PortalModule} from '@angular/cdk/portal';
 import {ScrollingModule} from '@angular/cdk/scrolling';
@@ -43,7 +42,6 @@ import {MatTreeModule} from '@angular/material/tree';
 
 @NgModule({
   exports: [
-    A11yModule,
     CdkStepperModule,
     CdkTableModule,
     CdkTreeModule,
